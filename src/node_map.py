e87a0409306dd0aee14a023eb95449dcfe47557e
Jonathan Stout <jonstout@indiana.edu>
2012-03-29 18:32:16 -0400

    dc.SetPen fix attempt.

--- src/node_map.py
+++ src/node_map.py
@@ -58,13 +58,15 @@ class NodeMap(wx.Panel):
             # Update Drawing code
             src_pos = link.srcpos
             dst_pos = link.dstpos
-            dc.SetPen(wx.Pen(wx.Colour(58,58,58), 2))
+            dc.SetPen(wx.Pen(wx.Colour(58,58,58)))
             
             if link.info or self.show_ports:
                 s = 'src_port: ' + str(link.srcport) + '\ndst_port: ' + str(link.dstport)
                 x = (src_pos[0]+dst_pos[0])/2 - 20
                 y = (src_pos[1]+dst_pos[1])/2 - 10
                 dc.DrawText(s, x, y)
+
+            dc.SetPen(wx.Pen(wx.Colour(58,58,58), 2))
             dc.DrawLine(src_pos[0], src_pos[1],
                         dst_pos[0], dst_pos[1])
 
